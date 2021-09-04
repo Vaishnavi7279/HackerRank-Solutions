7f1c7fdc5b037c3e7bc238d685c3834152818fd2
Vaishnavi7279 <srivastava93vaishnavi@gmail.com>
2021-09-04 23:57:40 +0530

    Updates

---
+++ Python/findthepercentage.py
@@ -0,0 +1,8 @@
+#HackerRank Problem
+#Finding the percentage
+num=int(input())
+for i in range(0,num):
+    diction.key[i]=string(input())
+    diction.value+input(diction.value[i])
+print(diction.keys())
+print(diction.values())
